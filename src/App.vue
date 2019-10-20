2fc5a3db3187529ab7fd533eeef0b3e815931ed6
macluckie <dimitri.macluckie@gmail.com>
2019-10-20 16:10:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <button
+          class="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li v-for="(nav_link, index) in nav_links" v-bind:key="index" class="nav-item">
+              <router-link class="nav-link" :to="nav_link.route">{{nav_link.name}}</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+
+export default { 
+  
+  name: "App",
+  data() {
+    return {
+      nav_links: [
+        { name: "Home", route: "/" },
+        { name: "About", route: "/about" },
+        {
+          name: "Article",
+          route: "/article/:id"
+        },
+        {
+          name: "Contact",
+          route: "/contact"
+        }
+      ]
+    };
+  },
+  mounted() {
+    import ('./assets/vendor/bootstrap/js/bootstrap.bundle.min.js')
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import Hometest from './views/Home.vue'
+import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+import Article from '@/components/Article'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+   
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/article/:id',
+      name: 'Article',
+      component: Article
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+  //  {
+    //  path: '/abouttest',
+      //name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    //}
+  ]
+})
